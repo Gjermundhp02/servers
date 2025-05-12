012ba063c391fc7392a3f19d9e28298156412947
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-05-12 23:34:38 +0200

    Changed to vma

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,23 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ 
+  config, pkgs, ... 
+}: {
+  boot.loader.efi.canTouchEfiVariables = true;
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-
-  # Enable networking
   networking.networkmanager.enable = true;
+  networking.hostName = "nixos"; # Define your hostname.
 
   # Set your time zone.
   time.timeZone = "Europe/Oslo";
@@ -26,64 +16,23 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "no";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "no";
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.gjermund = {
+  users.users.nixos = {
     isNormalUser = true;
-    description = "Gjermund";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [];
+    description = "nixos";
+    extraGroups = [ "networkmanager" "wheel" ];
     openssh.authorizedKeys.keys = let
       authorizedKeys = builtins.fetchurl {
         url = "https://github.com/gjermundhp02.keys";
-        sha256 = "sha256:1gc1c8syv70j25s8g8lifxh9prp9hn4hcf2kdbig4hihsi8hjrgw";
+        sha256 = "sha256:11j5fv71wwy04v2x6qqwn32b1xn0qmx5kips78fazqa5yi2i8w3f";
       };
-    in pkgs.lib.splitString "\n" (builtins.readFile
-    authorizedKeys);
+    in pkgs.lib.splitString "\n" (builtins.readFile authorizedKeys);
   };
 
-  environment.systemPackages = with pkgs; [
-  
-  ];
-
-  virtualisation.docker.enable = true;
-  virtualisation.oci-containers = {
-    backend = "docker";
-    containers = {
-      website = {
-        image = "ghcr.io/gjermundhp02/website:latest";
-        ports = [
-          "80:3000"
-        ];
-      };
-    };
-  };
-  
+  services.qemuGuest.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,17 +1,31 @@
 {
-  description = "Server configuration";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      server = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages.x86_64-linux = {
+      proxmox = nixos-generators.nixosGenerate {
         system = "x86_64-linux";
         modules = [
+          # you can include your own nixos configuration here, i.e.
           ./configuration.nix
         ];
+        format = "proxmox";
+        
+        # optional arguments:
+        # explicit nixpkgs and lib:
+        # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        # lib = nixpkgs.legacyPackages.x86_64-linux.lib;
+        # additional arguments to pass to modules:
+        # specialArgs = { myExtraArg = "foobar"; };
+        
+        # you can also define your own custom formats
+        # customFormats = { "myFormat" = <myFormatModule>; ... };
+        # format = "myFormat";
       };
     };
   };
